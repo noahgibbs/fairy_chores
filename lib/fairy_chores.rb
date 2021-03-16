1bb2522a89c0b629509a4839a0790885bfba1a7e
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-16 13:00:11 +0000

    Initial commit after bundle gem

---
+++ lib/fairy_chores.rb
@@ -0,0 +1,6 @@
+require "fairy_chores/version"
+
+module FairyChores
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fairy_chores/version.rb
@@ -0,0 +1,3 @@
+module FairyChores
+  VERSION = "0.1.0"
+end
